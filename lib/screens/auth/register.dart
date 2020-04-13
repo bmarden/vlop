305fe4bfddab4b8676d10b3abbf8e778c9df5351
Ben Marden <benvmarden@gmail.com>
2020-04-13 12:54:09 -0700

    Text box created

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vlop/services/auth_service.dart';
+import 'package:vlop/utilities/constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Register extends StatefulWidget {
   // Declare the function from auth_view. Will be used to change to login screen
@@ -37,6 +39,60 @@ class _RegisterState extends State<Register> {
                   Color(0xFF398AE5),
                 ],
                 stops: [0.1, 0.4, 0.7, 0.9],
+              ),
+            ),
+          ),
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                horizontal: 40.0,
+                vertical: 120.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "User Name",
+                        style: customLabelStyle,
+                      ),
+                      SizedBox(height: 10.0),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        decoration: customBoxStyle,
+                        height: 60.0,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.only(top: 14.0),
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.grey,
+                            ),
+                            hintText: "Enter a user name",
+                            hintStyle: customHintText,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
