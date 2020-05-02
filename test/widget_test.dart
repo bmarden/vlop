73a5870070b6ed7f79077c759fad5fd1ddf55a83
Ben Marden <benvmarden@gmail.com>
2020-05-01 20:20:11 -0700

    Add travisCI yml, basic initial test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:vlop/main.dart';
+import 'package:vlop/models/user.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  // Build our app and trigger a frame.
+  group('User test', () {
+    test('Test User declaration', () {
+      User newUser =
+          User(uid: '1234', userName: 'testUser', email: 'testUser@email.com');
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      expect(newUser.uid, '1234');
+      expect(newUser.userName, 'testUser');
+      expect(newUser.email, 'testUser@email.com');
+    });
   });
 }
