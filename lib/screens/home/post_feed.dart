053ef73f61cff2d69a8d6db697d6ae5c1ba0f79b
Ben Marden <benvmarden@gmail.com>
2020-05-02 01:34:05 -0700

    Basic feed view showing photos

--- lib/screens/home/post_feed.dart
+++ lib/screens/home/post_feed.dart
@@ -1,19 +1,40 @@
-import 'dart:typed_data';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vlop/models/photo.dart';
-import 'package:vlop/services/database.dart';
+import 'package:vlop/utilities/loading.dart';
+import 'dart:io';
 
 class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var pics = Provider.of<List<Photo>>(context);
-    pics.forEach((pic) => print(pic.id));
-    return ListView(
-      children: <Widget>[],
+    return pics == null
+        ? Loading()
+        : ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: pics.length,
+            itemBuilder: (context, index) {
+              return _imgTile(pics[index]);
+            },
+          );
+  }
+
+  Widget _imgTile(Photo curPhoto) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: ListTile(
+        title: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text('posted by: ${curPhoto.userOwner}'),
+            ),
+            Image.network(curPhoto.url),
+          ],
+        ),
+      ),
     );
   }
 }
