6bce52292cb2e054ff26f3fb32852b471b463a46
Ben Marden <benvmarden@gmail.com>
2020-05-09 23:26:43 -0700

    Add some styling to post feed

--- lib/screens/home/post_feed.dart
+++ lib/screens/home/post_feed.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vlop/models/photo.dart';
 import 'package:vlop/utilities/loading.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Feed extends StatelessWidget {
   @override
@@ -13,12 +14,12 @@ class Feed extends StatelessWidget {
             scrollDirection: Axis.vertical,
             itemCount: pics.length,
             itemBuilder: (context, index) {
-              return _imgTile(pics[index]);
+              return _imgTile(context, pics[index]);
             },
           );
   }
 
-  Widget _imgTile(Photo curPhoto) {
+  Widget _imgTile(context, Photo curPhoto) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12.0),
@@ -30,10 +31,31 @@ class Feed extends StatelessWidget {
               padding: const EdgeInsets.all(10.0),
               child: Text('posted by: ${curPhoto.userOwner}'),
             ),
-            Image.network(curPhoto?.url,
-                loadingBuilder: (context, child, progress) {
-              return progress == null ? child : CircularProgressIndicator();
-            }),
+            SizedBox(
+              height: 300.0,
+              child: Center(
+                child: CachedNetworkImage(
+                  imageUrl: curPhoto?.url,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 4,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      CircularProgressIndicator(
+                          value: downloadProgress.progress),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+            ),
           ],
         ),
       ),
